43d1fc510436d5dfd8a6841b928c29f1365f92fc
Benjamin <Wormss9.bd@gmail.com>
2020-05-08 21:41:39 +0200

    Grammar check.

--- src/main/java/com/github/repron/vote/commands/CommandVote.java
+++ src/main/java/com/github/repron/vote/commands/CommandVote.java
@@ -1,24 +1,24 @@
-package org.github.repron.vote.commands;
+package com.github.repron.vote.commands;
 
-import org.github.repron.vote.Vote;
+import com.github.repron.vote.Vote;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CommandVote implements CommandExecutor {
 
-    public Vote plugin;
+    private Vote plugin;
 
-    public CommandVote(Vote plugin){
+    public CommandVote(Vote plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length == 1) {
-            plugin.getLogicVote().sendCommand(sender,args[0]);
+        if (args.length == 1) {
+            plugin.getLogicVote().sendCommand(sender, args[0]);
         } else {
-            sender.sendMessage("nespravny pocet argumentov");
+            sender.sendMessage("Nesprávny počet argumentov");
         }
         return false;
 
